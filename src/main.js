523ca623eb9f965524dd5854f8b73a49a6f710c3
cunnellp5 <philip.cunnell@colorado.edu>
2017-03-13 20:17:33 -0600

    first day capstone

---
+++ src/main.js
@@ -0,0 +1,24 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import navbar from './components/navbar'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  template: `
+  <div>
+    <navbar />
+    <section class="section">
+      <div class="container is-fluid">
+      </div>
+    </section>
+  </div>
+  `,
+  components: {
+    navbar
+  }
+}).$mount('#app')
